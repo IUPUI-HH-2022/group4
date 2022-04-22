af48198765f8db00118ef54ef6f0d7d038ab9890
Arcorvin <77901990+Arcorvin@users.noreply.github.com>
2022-04-22 02:16:58 -0400

    Update accessability.js

--- scripts/accessability.js
+++ scripts/accessability.js
@@ -18,3 +18,45 @@ function increaseFontSize() {
 function decreaseFontSize() {
     //TODO
 }
+
+//adds the function to click a button and it will either show or hide words
+
+function toggleText() {
+    var text = document.getElementById("disappear");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+    
+  }
+
+  function toggleText2() {
+    var text = document.getElementById("disappear2");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+    
+  }
+
+  function toggleText3() {
+    var text = document.getElementById("disappear3");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+    
+  }
+
+  function toggleText4() {
+    var text = document.getElementById("disappear4");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+    
+  }
